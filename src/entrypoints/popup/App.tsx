a8e9c68204e0311ec1ac494a77ed12b5baaa3388
tomo-local <shinsi4545@gmail.com>
2025-02-08 23:07:08 +0900

    Popupコンポーネントを更新: 検索機能を強化し、タブ、履歴、ブックマークの検索結果を統合

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,156 +1,42 @@
 import { useEffect, useRef, useState } from "react";
+
 import SearchBar from "@/components/SearchBar";
 import ResultLine from "@/components/ResultLine";
 
-type TabResult = chrome.tabs.Tab & { type: "tab" };
-type HistoryResult = chrome.history.HistoryItem & { type: "history" };
+import { ListContext } from "@/machine/searchList";
+import {
+  searchHistorySearch,
+  searchSuggestions,
+  searchTabSearch,
+  searchBookmarkSearch,
+} from "@/function/search";
 
 const Popup = () => {
-  const [tabs, setTabs] = useState<TabResult[]>([]);
-  const [history, setHistory] = useState<HistoryResult[]>([]);
+  const [list, setList] = useState<ListContext[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const listRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
-    handleTabSearch("", 5);
+    init();
   }, []);
 
-  const handleTabSearch = async (query: string, count: number) => {
-    const allWindows = await chrome.windows.getAll({
-      windowTypes: ["normal"],
+  const init = async () => {
+    const newList = await searchTabSearch({
+      query: "",
+      option: { count: 4 },
     });
 
-    const notFocusedWindowIds = allWindows.map((window) => window.id);
+    const listContext = newList.map((tab) => ({
+      type: "tab",
+      title: tab?.title || "",
+      icon: tab?.favIconUrl,
+      id: tab.id,
+    })) as ListContext[];
 
-    const currentWindows = await chrome.windows.getCurrent();
-
-    if (currentWindows) {
-      notFocusedWindowIds.unshift(currentWindows.id);
-    }
-
-    const allTabs = await Promise.all(
-      [...new Set(notFocusedWindowIds)].map((windowId) =>
-        chrome.tabs.query({ windowId })
-      )
-    ).then((results) => results.flat().map((tab) => ({ ...tab, type: "tab" })));
-
-    if (query === "") {
-      // @ts-ignore
-      setTabs(allTabs);
-      return;
-    }
-
-    const exactMatches = allTabs.filter((tab) =>
-      tab?.title?.toLowerCase().includes(query.toLowerCase())
-    );
-
-    if (exactMatches.length === 0) {
-      setTabs([]);
-      return;
-    }
-
+    setList(listContext);
     setSelectedIndex(0);
-
-    if (count) {
-      // @ts-ignore
-      setTabs([...exactMatches.slice(0, count)]);
-      return;
-    }
-
-    // @ts-ignore
-    setTabs([...exactMatches]);
   };
-
-  const handleHistorySearch = async (query: string) => {
-    if (query === "") {
-      setHistory([]); // 空文字の場合は履歴を表示しない
-      return;
-    }
-    const splitQuery = query.split("|").map((q) => q.trim());
-
-    await Promise.all(
-      splitQuery.map((q) =>
-        chrome.history.search(
-          {
-            text: q,
-            startTime: new Date().setDate(new Date().getDate() - 30),
-            endTime: new Date().getTime(),
-            maxResults: 20,
-          },
-          (results) => {
-            // @ts-ignore
-            setHistory((prev) => [
-              ...prev,
-              ...uniqueObjects(results).filter(
-                (item) =>
-                  item.title?.toLowerCase().includes(q.toLowerCase()) ||
-                  item.url?.toLowerCase().includes(q.toLowerCase())
-              ),
-            ]);
-          }
-        )
-      )
-    );
-  };
-
-  function uniqueObjects(
-    arr: chrome.history.HistoryItem[]
-  ): chrome.history.HistoryItem[] {
-    const seen = new Set();
-    const result = [];
-
-    for (const obj of arr) {
-      const key = obj.title || obj.url; // titleまたはurlをキーとして使用
-
-      if (!seen.has(key)) {
-        seen.add(key);
-        result.push(obj);
-      }
-    }
-
-    return result;
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-    handleTabSearch(e.target.value, 10);
-    handleHistorySearch(e.target.value); // 入力内容に応じて履歴検索も実行
-    setSelectedIndex(-1); // 検索時に選択状態をリセット
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && selectedIndex >= 0) {
-      const item = combinedResults[selectedIndex];
-      if (item.type === "tab") {
-        chrome.tabs.update(item.id as number, { active: true });
-        window.close();
-      } else {
-        window.open(item.url, "_blank");
-      }
-      setSelectedIndex(-1); // 選択後、選択状態をリセット
-    } else if (e.key === "ArrowDown") {
-      setSelectedIndex((prevIndex) =>
-        Math.min(prevIndex + 1, combinedResults.length - 1)
-      );
-    } else if (e.key === "ArrowUp") {
-      setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-    }
-  };
-
-  const handleMouseEnter = (index: number) => {
-    setSelectedIndex(index);
-  };
-
-  const getCombinedResults = () => {
-    const combined = [
-      ...tabs.map((tab) => ({ ...tab, type: "tab" })),
-      ...history.map((item) => ({ ...item, type: "history" })),
-    ];
-    return combined; // 候補数制限を削除
-  };
-
-  const combinedResults = getCombinedResults();
 
   useEffect(() => {
     if (listRef.current && selectedIndex >= 0) {
@@ -161,6 +47,147 @@ const Popup = () => {
       });
     }
   }, [selectedIndex]);
+
+  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    setSearchTerm(query);
+
+    const newList = await searchList(query, {
+      suggestionCount: 5,
+      historyCount: 10,
+      tabCount: 10,
+    });
+
+    setList(newList);
+  };
+
+  const searchList = async (
+    query: string,
+    option: {
+      historyCount?: number;
+      tabCount?: number;
+      suggestionCount?: number;
+    }
+  ) => {
+    const suggestionResult = await searchSuggestions({
+      query,
+      option: { count: option.suggestionCount || 5 },
+    });
+
+    const tabResult = await searchTabSearch({
+      query,
+      option: { count: option.tabCount || 10 },
+    });
+
+    const historyResult = await searchHistorySearch({
+      query,
+      option: {
+        count: option.historyCount || 10,
+        term: {
+          start: new Date().getTime() - 1000 * 60 * 60 * 24 * 30, // 30日前から
+          end: new Date().getTime(), // 現在まで
+        },
+      },
+    });
+
+    const bookmarkResult = await searchBookmarkSearch({
+      query,
+      option: { count: 5 },
+    });
+
+    const list = [
+      ...suggestionResult.map((item) => ({
+        type: "search",
+        title: item,
+        url: `https://www.google.com/search?q=${item}`,
+      })),
+      ...tabResult.map((tab) => ({
+        type: "tab",
+        title: tab?.title || "",
+        icon: tab?.favIconUrl,
+        id: tab.id,
+      })),
+      ...historyResult.map((history) => ({
+        type: "history",
+        title: history.title || "",
+        url: history.url,
+      })),
+      ...bookmarkResult.map((bookmark) => ({
+        type: "bookmark",
+        title: bookmark.title,
+        url: bookmark.url,
+      })),
+    ] as ListContext[];
+
+    return rankResults(list);
+  };
+
+  const rankResults = (result: ListContext[]) => {
+    const ranked = result.sort((a, b) => {
+      //完全一致を最優先
+      const aTitle = a.title?.toLowerCase() || "";
+      const bTitle = b.title?.toLowerCase() || "";
+
+      const aExactMatch = aTitle === searchTerm.toLowerCase();
+      const bExactMatch = bTitle === searchTerm.toLowerCase();
+      if (aExactMatch && !bExactMatch) return -1;
+      if (!aExactMatch && bExactMatch) return 1;
+
+      // 次に部分一致
+      const aPartialMatch = aTitle.includes(searchTerm.toLowerCase());
+      const bPartialMatch = bTitle.includes(searchTerm.toLowerCase());
+
+      if (aPartialMatch && !bPartialMatch) return -1;
+      if (!aPartialMatch && bPartialMatch) return 1;
+
+      // タブ、履歴、検索候補の優先順位
+      const typeOrder = {
+        tab: 0,
+        history: 1,
+        search: 2,
+        bookmark: 3,
+      };
+
+      if (a.type === b.type) {
+        return 0;
+      }
+
+      return typeOrder[a.type] - typeOrder[b.type];
+    });
+
+    return ranked;
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log("========", e);
+    // コマンドキーとの組み合わせを考慮
+    if (e.key === "Enter" && selectedIndex >= 0) {
+      const item = list[selectedIndex];
+      if (item.type === "tab") {
+        chrome.tabs.update(item.id as number, { active: true });
+        window.close();
+      } else {
+        window.open(item.url, "_blank");
+      }
+      setSelectedIndex(-1); // 選択後、選択状態をリセット
+    }
+
+    if ((e.metaKey || e.ctrlKey) && e.key === "t") {
+      e.preventDefault();
+      window.close();
+    }
+
+    if (e.key === "ArrowDown") {
+      setSelectedIndex((prevIndex) => Math.min(prevIndex + 1, list.length - 1));
+    }
+    if (e.key === "ArrowUp") {
+      setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    }
+  };
+
+  const handleMouseEnter = (index: number) => {
+    setSelectedIndex(index);
+  };
 
   return (
     <div className="px-4 pt-2 pb-3 font-sans text-white bg-gray-800 w-[700px]">
@@ -176,10 +203,9 @@ const Popup = () => {
           ref={listRef}
           className="overflow-y-scroll max-h-[400px] hidden-scrollbar"
         >
-          {combinedResults.map((item, index) => (
+          {list.map((item, index) => (
             <ResultLine
               key={`${index.toString()}`}
-              // @ts-ignore
               item={item}
               index={index}
               selectedIndex={selectedIndex}
