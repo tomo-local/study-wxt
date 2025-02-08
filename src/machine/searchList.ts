2e7278250f4d2c589a5a3f9399ecc2e95d093173
tomo-local <shinsi4545@gmail.com>
2025-02-08 23:06:48 +0900

    検索リストのコンテキスト型を追加: 検索、タブ、履歴、ブックマークの型を定義

---
+++ src/machine/searchList.ts
@@ -0,0 +1,32 @@
+type ListContext =
+  | SearchContext
+  | TabContext
+  | HistoryContext
+  | BookmarkContext;
+
+type SearchContext = {
+  type: "search";
+  title: string;
+  url: string;
+};
+
+type TabContext = {
+  type: "tab";
+  title: string;
+  icon: string;
+  id: number;
+};
+
+type HistoryContext = {
+  type: "history";
+  title: string;
+  url: string;
+};
+
+type BookmarkContext = {
+  type: "bookmark";
+  title: string;
+  url: string;
+};
+
+export { ListContext };
