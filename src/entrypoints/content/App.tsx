9732309767710a1ff12c2a1a4bf5c7a64f8ccbdf
tomo-local <shinsi4545@gmail.com>
2025-03-20 00:59:38 +0900

    componentのファイルの位置を変更

--- src/entrypoints/content/App.tsx
+++ src/entrypoints/content/App.tsx
@@ -1,22 +1,20 @@
 import { useState, useEffect } from "react";
 import "@/assets/global.css";
-import SearchBar from "@/components/SearchBar";
+import SearchBar from "@/components/common/SearchBar";
 export default function App() {
   const [open, setOpen] = useState(false);
 
   const sendMessage = () => {
-    chrome.runtime.sendMessage({ type: "CLOSE_POPUP" }).then(e => {
-      console.log("sendMessage", e);
-    }).catch((err) => {
-      console.error("open popup", err)
+    chrome.runtime.sendMessage({ type: "CLOSE_POPUP" }).catch((err) => {
+      console.error("close popup", {
+        error: err,
+      });
     });
   };
 
   const onClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log("popup close");
-
     sendMessage();
   };
 
@@ -34,7 +32,7 @@ export default function App() {
         <div className="flex items-center justify-center min-h-full p-4 text-center">
           <div className="relative w-full max-w-3xl overflow-hidden text-left transition-all transform">
             <div
-              className="px-4 px-6 py-3 bg-gray-800 border border-gray-600 border-solid rounded-lg shadow-xl"
+              className="px-4 px-6 py-3 text-gray-100 bg-gray-800 border-2 border-gray-300 border-solid rounded-lg shadow-xl"
               onClick={(e) => {
                 e.stopPropagation();
                 console.log("clicked");
