ca53396ee3564d1a5070f5c13cc267c0343213d4
tomo-local <shinsi4545@gmail.com>
2025-03-20 01:31:40 +0900

    chrome.tsファイルを新規作成し、ポップアップのオープンおよびクローズ機能を実装。メッセージタイプとアクションタイプの列挙型を追加。

---
+++ src/function/chrome.ts
@@ -0,0 +1,48 @@
+enum MessageType {
+  CLOSE_POPUP = "CLOSE_POPUP",
+  OPEN_POPUP = "OPEN_POPUP",
+}
+
+enum ActionType {
+  runtime = "runtime",
+  tabs = "tabs",
+}
+
+const actionRunContent = (message: MessageType) => {
+  chrome.runtime.sendMessage({ type: message }).catch((err) => {
+    // TODO: エラー時に通知を出して、拡張機能を再度インストールするように促す
+  });
+};
+
+const actionTabsContent = async (message: MessageType) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tabId = tabs[0].id;
+    if (tabId) {
+      chrome.tabs.sendMessage(tabId, { type: message }).catch((err) => {
+        // TODO: エラー時に通知を出して、拡張機能を再度インストールするように促す
+      });
+    }
+  });
+};
+
+const closeContent = (type: ActionType) => {
+  if (type === ActionType.runtime) {
+    return actionRunContent(MessageType.CLOSE_POPUP);
+  }
+
+  if (type === ActionType.tabs) {
+    return actionTabsContent(MessageType.CLOSE_POPUP);
+  }
+};
+
+const openContent = async (type: ActionType) => {
+  if (type === ActionType.runtime) {
+    return actionRunContent(MessageType.OPEN_POPUP);
+  }
+
+  if (type === ActionType.tabs) {
+    return actionTabsContent(MessageType.OPEN_POPUP);
+  }
+};
+
+export { closeContent, openContent, ActionType, MessageType };
