e87bbb3f67f9a13f2b0283ea39d9d4c82b4df875
tomo-local <shinsi4545@gmail.com>
2025-03-21 01:54:06 +0900

    chrome.tsにタブ作成機能を追加し、関連する型を拡張

--- src/types/chrome.ts
+++ src/types/chrome.ts
@@ -2,6 +2,7 @@ export enum MessageType {
   CLOSE_POPUP = "CLOSE_POPUP",
   OPEN_POPUP = "OPEN_POPUP",
   QUERY_TAB = "QUERY_TAB",
+  CREATE_TAB = "CREATE_TAB",
   UPDATE_TAB = "UPDATE_TAB",
   REMOVE_TAB = "REMOVE_TAB",
 }
@@ -19,6 +20,7 @@ export interface Tab {
   active: boolean;
   lastAccessed: number;
   windowId: number;
+  currentWindow: boolean;
 }
 
 export type QueryOption = Pick<chrome.tabs.QueryInfo, "currentWindow"> & {
@@ -31,8 +33,18 @@ export interface QueryMessage {
   count?: number;
 }
 
+export interface CreateMessage {
+  type: MessageType.CREATE_TAB;
+  url: string;
+}
+
 export interface UpdateMessage {
   type: MessageType.UPDATE_TAB;
   tabId: number;
   windowId?: number;
+}
+
+export interface RemoveMessage {
+  type: MessageType.REMOVE_TAB;
+  tabId: number;
 }
