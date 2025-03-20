3f218718e95bfc0d3cafe05e630f09cd6d4fa424
tomo-local <shinsi4545@gmail.com>
2025-03-20 23:17:47 +0900

    SearchInputコンポーネントにMagnifyingGlassIconを追加し、検索結果がない場合のメッセージを表示するように改善

--- src/entrypoints/content/App.tsx
+++ src/entrypoints/content/App.tsx
@@ -1,5 +1,6 @@
 import "@/assets/global.css";
 import { useState, useRef, useEffect } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 import useTabSearch from "@/hooks/useTabSearch";
 
@@ -13,8 +14,7 @@ export default function App() {
   const [query, setQuery] = useState("");
   const tabs = useTabSearch(query);
 
-  const handleClose = (e: React.MouseEvent) => closeContent(ActionType.runtime);
-  const handleEscapeKey = () => closeContent(ActionType.runtime);
+  const handleClose = () => closeContent(ActionType.runtime);
 
   // TODO： arrow keyのhookを作成する
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -53,14 +53,21 @@ export default function App() {
       <ModalContainer className="w-full max-w-3xl min-h-48">
         <div className="px-6 py-2 text-gray-200 bg-gray-800 border-2 border-solid rounded-lg shadow-xl border-sky-500">
           <SearchInput
+            className="text-gray-200 bg-gray-800 focus:ring-sky-500"
+            leftContent={
+              <MagnifyingGlassIcon className="text-gray-400 size-6" />
+            }
             onChange={(e) => setQuery(e.target.value)}
-            onEscapeKeyDown={handleEscapeKey}
             onArrowUpDownKeyDown={handleArrowUpDownKey}
             onEnterKeyDown={handleEnterKey}
+            onEscapeKeyDown={handleClose}
           />
           <div className="pt-3 pb-2 mt-2 border-t border-gray-700 border-solid">
-            {tabs?.length && (
-              <ul className="overflow-y-auto hidden-scrollbar max-h-48" ref={listRef}>
+            {tabs?.length ? (
+              <ul
+                className="overflow-y-auto hidden-scrollbar max-h-48"
+                ref={listRef}
+              >
                 {tabs.map((item, index) => (
                   <ResultLine
                     key={item.id}
@@ -69,6 +76,8 @@ export default function App() {
                   />
                 ))}
               </ul>
+            ) : (
+              <p className="text-center text-gray-400">No results found</p>
             )}
           </div>
         </div>
