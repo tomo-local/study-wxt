291be076d7e57b2eb76be6650a3cdc14db8e2428
tomo-local <shinsi4545@gmail.com>
2025-03-21 01:42:15 +0900

    chrome.tsの型を整理し、タブ操作の機能を追加

---
+++ src/function/chrome/tab.ts
@@ -0,0 +1,63 @@
+import { Tab, QueryOption } from "@/types/chrome";
+
+const actionQuery = async (
+  query: string,
+  option: chrome.tabs.QueryInfo
+): Promise<chrome.tabs.Tab[]> => {
+  const response = await chrome.tabs.query(option);
+
+  if (!query) {
+    return response;
+  }
+
+  return response.filter((tab) => {
+    const title = tab.title || "";
+    const url = tab.url ? new URL(tab.url).hostname : "";
+
+    const isTitleMatch = title.toLowerCase().includes(query.toLowerCase());
+    const isUrlMatch = url.toLowerCase().includes(query.toLowerCase());
+
+    return isTitleMatch || isUrlMatch;
+  }) as chrome.tabs.Tab[];
+};
+
+const queryTabs = async (query: string, option: QueryOption) => {
+  const response = await actionQuery(query, {
+    currentWindow: option.currentWindow,
+  });
+
+  const tabs = response
+    .map((tab) => {
+      return {
+        id: tab.id,
+        title: tab.title || "",
+        url: tab.url || "",
+        icon: tab.favIconUrl || "",
+        active: tab.active || false,
+        lastAccessed: tab.lastAccessed || 0,
+        windowId: tab.windowId || 0,
+      } as Tab;
+    })
+    .sort((a, b) => b.lastAccessed - a.lastAccessed);
+
+  return option.count ? tabs.slice(0, option.count) : tabs;
+};
+
+const updateTab = async ({
+  tabId,
+  windowId,
+}: {
+  tabId: number;
+  windowId?: number;
+}) => {
+  await chrome.tabs.update(tabId, { active: true });
+
+  // Focus on the window
+  if (windowId) {
+    await chrome.windows.update(windowId, { focused: true });
+  }
+};
+
+const removeTab = async (tabId: number) => await chrome.tabs.remove(tabId);
+
+export { queryTabs, updateTab, removeTab };
--- src/hooks/useTabSearch.ts
+++ src/hooks/useTabSearch.ts
@@ -1,20 +1,23 @@
 import { useState, useEffect } from "react";
+import { Tab, MessageType } from "@/types/chrome";
 
-interface Tab {
-  id: number;
-  title: string;
-  url: string;
-  icon: string;
-}
+const DEFAULT_COUNT = 3;
 
 export default function useTabSearch(query: string) {
   const [filteredTabs, setFilteredTabs] = useState<Tab[]>([]);
+  const [count, setCount] = useState<number | undefined>(DEFAULT_COUNT);
 
   useEffect(() => {
-    chrome.runtime.sendMessage({ type: "SEARCH_TAB", query }, (response) => {
-      setFilteredTabs(response.result);
-    });
+    chrome.runtime.sendMessage(
+      { type: MessageType.QUERY_TAB, query, count },
+      (response) => {
+        setFilteredTabs(response.result);
+      }
+    );
+    setCount(undefined);
   }, [query]);
 
-  return filteredTabs;
+  return {
+    tabs: filteredTabs,
+  };
 }
